738be7867f0e09c7a0c9cf05ffcfb64cdded1c6b
Hemanth <hemanthdevulapalli8@gmail.com>
2026-06-14 18:34:44 +0530

    fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,44 @@
+// require("dotenv").config();
+
+// console.log("=================================");
+// console.log("ENV CHECK");
+// console.log("PORT:", process.env.PORT);
+// console.log("MONGO_URI:", process.env.MONGO_URI);
+// console.log("JWT_SECRET:", process.env.JWT_SECRET);
+// console.log("=================================");
+
+// const express = require("express");
+// const cors = require("cors");
+
+// const connectDB = require("./config/db");
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Connect Database
+// connectDB();
+
+// // Routes
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/leaves", require("./routes/leaveRoutes"));
+
+// // Test Route
+// app.get("/", (req, res) => {
+//   res.send("Leave Management System API Running");
+// });
+
+// // Start Server
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
 require("dotenv").config();
 
 console.log("=================================");
@@ -9,13 +50,20 @@ console.log("=================================");
 
 const express = require("express");
 const cors = require("cors");
-
 const connectDB = require("./config/db");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://leave-management-system-alpha-one.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 // Connect Database
@@ -25,7 +73,7 @@ connectDB();
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/leaves", require("./routes/leaveRoutes"));
 
-// Test Route
+// Health Check Route
 app.get("/", (req, res) => {
   res.send("Leave Management System API Running");
 });
